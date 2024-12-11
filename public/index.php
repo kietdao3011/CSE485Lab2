2a97fa3cb268ae05eb4340f9cac2e0efb33014c5
BinnyHa <bien.ha310898@gmail.com>
2024-12-11 16:08:04 +0700

    update

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ require_once('../tlunews/config/config.php');
 require_once APP_ROOT.'/tlunews/libs/dbconnection.php';
 
 
+
 $controller = isset($_GET['controller']) ? $_GET['controller'] :'home';
 $action = isset($_GET['action']) ? $_GET['action'] :'index';
 
---
+++ tlunews/views/user/edit.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+<div class="input-group w-50 gap-3">
+  <div class="col">
+    <input type="text" class="form-control " placeholder="Username" aria-label="Username">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control " placeholder="Password" aria-label="Password">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control " placeholder="Role" aria-label="Role">
+  </div>
+  <input class="btn btn-primary" type="submit" value="Submit">
+</div>
+
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
